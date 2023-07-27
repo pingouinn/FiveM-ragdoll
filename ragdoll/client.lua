c9dbcef5bfcd5f617ad15f1cb3af6d42a56867de
Hysteryx Théo.Sicot <theo.sicot.dev@gmail.com>
2023-07-27 14:17:37 +0200

    Smoothification et optimisation

--- ragdoll/client.lua
+++ ragdoll/client.lua
@@ -2,52 +2,69 @@
 -- Variables --
 ---------------
 
--- World related variables
-
-local ped
-
 -- State variables
 
-local onGround
+local ragdolled = false 
 
 -- Memory load functions
 
 LoadAnim = function(dict)
+	RequestAnimDict(dict)
 	while not HasAnimDictLoaded(dict) do
-		RequestAnimDict(dict)
-		
-		Citizen.Wait(1)
+		Wait(0)
 	end
 end
 
 ----------
--- Code --
+-- Mapper --
 ----------
 
-Citizen.CreateThread(function()
-	while true do
-		ped = PlayerPedId()
-		if IsControlJustPressed(0, cfg.key) then
-			ragdoll()
-		end
-		Citizen.Wait(3)
-	end
-end) 
+RegisterKeyMapping(config.commandName,config.mapperDesc,config.defaultMapper,config.defaultParameter)
+RegisterCommand(config.commandName, function()
+	ragdoll()
+end)
 
 ---------------
 -- Functions --
 ---------------
 
 function ragdoll()
-	if not onGround then
-		SetPedToRagdoll(ped, 300000, 300000, 0, 0, 0, 0)
-		onGround = not onGround
+	local ped = PlayerPedId()
+	if not CanPedRagdoll(ped) or IsEntityDead(ped) or IsPedInAnyVehicle(ped,false) then 
+		ragdoll = false 
+		return 
 	else 
+		Ragdoll = not Ragdoll 
+	end 
+
+	if Ragdoll then
+		ShakeGameplayCam(config.cameraEffect, 1.0)
+		for i = 0, 10 do 
+			SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+			Wait(200)
+		end 
+		while GetEntitySpeed(ped) > 0.1 do 
+			SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
+			Wait(200)
+		end 
 		ClearPedTasksImmediately(ped)
-		SetEntityHeading(ped, GetEntityHeading(ped) + 180.0)
-		LoadAnim("missheist_agency3astumble_getup")
-        TaskPlayAnim(ped, "missheist_agency3astumble_getup" ,"stumble_getup", 8.0, -8.0, -1, 0, 0, false, false, false)
+		LoadAnim(config.fallAnimDict)
+		local coords = GetEntityCoords(ped)
+		while not IsEntityPlayingAnim(ped,config.fallAnimDict, config.fallAnim ,1) do 
+			Wait(0)
+			TaskPlayAnimAdvanced(ped,config.fallAnimDict, config.fallAnim ,coords.x,coords.y,coords.z,0.0,0.0,0.0,1.0,1.0,-1,1,0.9,0,0)
+		end 
+	else 
+		ShakeGameplayCam(config.cameraEffect, 1.0)
+		LoadAnim(config.UpAnimDict)
+        TaskPlayAnim(ped, config.UpAnimDict ,config.UpAnim, 8.0, -8.0, -1, 0, 0, false, false, false)
         Wait(4500)
-		onGround = not onGround
-	end
+		if config.Drunk  then 
+			LoadAnim("move_m@drunk@moderatedrunk")
+			TaskPlayAnim(ped,"move_m@drunk@moderatedrunk","idle",1.0,1.0,config.DrunkCooldown,1,0,false,false,false)
+		end 
+	end 
+	RemoveAnimDict(config.UpAnim)
+	RemoveAnimDict(config.UpAnim)
 end
+
--- ragdoll/config.lua
+++ ragdoll/config.lua
@@ -2,13 +2,29 @@
 -- Config file --
 -----------------
 
-cfg = {}
+config = {}
+
+config.cameraEffect = "DEATH_FAIL_IN_EFFECT_SHAKE"
+
+--All animations : https://gtahash.ru/animations/
+
+config.fallAnimDict = "dead"
+config.fallAnim = "dead_d"
+
+config.UpAnimDict = "missheist_agency3astumble_getup"
+config.UpAnim = "stumble_getup"
+
+
+config.Drunk = true 
+config.DrunkCooldown = 3000
 
 -------------------- KEYS -----------------------
 
--- Key to press to ragdoll.
--- Key "Numpad 8" (111) by default
--- Site listing controls: https://docs.fivem.net/docs/game-references/controls/#controls
--- KEY NUMBER = INDEX COLUMN
+config.commandName = "ragdoll"
+config.mapperDesc = "Faire tomber le ped (ragdoll)"
+config.defaultMapper = "keyboard"
+config.defaultParameter = "F7" 
 
-cfg.key = 111
+-- Controls : 
+-- https://docs.fivem.net/docs/game-references/controls/
+-- 'Name'
